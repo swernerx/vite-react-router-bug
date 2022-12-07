1a38b7f177b629fa224e4141509c4ed6a123443b
Sebastian Werner <s.werner@sebastian-software.de>
2022-12-07 10:32:57 +0100

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import { Routes } from 'generouted/react-router'
 
 import './App.css'
-import { Link } from 'react-router-dom'
 
-Link
+// Work-around to fix Vite import warning
+// import { Link } from 'react-router-dom'
+// Link
 
 function App() {
   return (
