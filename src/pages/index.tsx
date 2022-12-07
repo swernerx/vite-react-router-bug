473729dff5dc50b90a230a77b114303cef08e880
Sebastian Werner <s.werner@sebastian-software.de>
2022-12-07 10:27:14 +0100

    feat: added generouted

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function About() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="about">Link to About</Link>
+    </div>
+  )
+}
